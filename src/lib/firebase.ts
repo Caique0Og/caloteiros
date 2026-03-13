80c928ca6f4fcff60998a09efc4e4bf84336061b
Emily Rafaela Teixeira Silva <2401584@SALAS.AULAS>
2026-03-13 12:01:29 -0300

    feat: atualiza credenciais Firebase para branch emily

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAwdXT30l4uFfYXT0774xP8JUD7cMIKsI",
-  authDomain: "homologcaloteiros-5170f.firebaseapp.com",
-  projectId: "homologcaloteiros-5170f",
-  storageBucket: "homologcaloteiros-5170f.firebasestorage.app",
-  messagingSenderId: "153012378956",
-  appId: "1:153012378956:web:59d52571e83a16494583e9",
-  measurementId: "G-F59XXCYP0N"
+  apiKey: 'AIzaSyBdEdxRBBbeMGxHLUQYqXE9y7iectmE8fU',
+  authDomain: 'caloteiros-login.firebaseapp.com',
+  projectId: 'caloteiros-login',
+  storageBucket: 'caloteiros-login.firebasestorage.app',
+  messagingSenderId: '93529359374',
+  appId: '1:93529359374:web:e106cfa879ce4c8d3d285f',
+  measurementId: 'G-VNBMGFHHJD'
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export { app };
